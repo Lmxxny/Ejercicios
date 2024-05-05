5d75b5d382baab602ba9188739a6f37af829b347
Park Jihoon <trabajostodo0@gmail.com>
2024-05-05 09:36:28 -0600

    ejercicio 4

---
+++ Raiz Cuadrada/Raiz Cuadrada/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Raiz_Cuadrada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.btncalc = new System.Windows.Forms.Button();
+            this.btncerrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtx = new System.Windows.Forms.TextBox();
+            this.txtraiz = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnlimpiar.Location = new System.Drawing.Point(49, 161);
+            this.btnlimpiar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(92, 47);
+            this.btnlimpiar.TabIndex = 0;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = false;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // btncalc
+            // 
+            this.btncalc.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncalc.Location = new System.Drawing.Point(151, 161);
+            this.btncalc.Margin = new System.Windows.Forms.Padding(5);
+            this.btncalc.Name = "btncalc";
+            this.btncalc.Size = new System.Drawing.Size(107, 47);
+            this.btncalc.TabIndex = 1;
+            this.btncalc.Text = "Calcular";
+            this.btncalc.UseVisualStyleBackColor = false;
+            this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
+            // 
+            // btncerrar
+            // 
+            this.btncerrar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncerrar.Location = new System.Drawing.Point(268, 161);
+            this.btncerrar.Margin = new System.Windows.Forms.Padding(5);
+            this.btncerrar.Name = "btncerrar";
+            this.btncerrar.Size = new System.Drawing.Size(102, 47);
+            this.btncerrar.TabIndex = 2;
+            this.btncerrar.Text = "Cerrar";
+            this.btncerrar.UseVisualStyleBackColor = false;
+            this.btncerrar.Click += new System.EventHandler(this.btncerrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 14.26415F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Calcular la raíz cuadrada";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingrese un número:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Raíz Cuadrada: ";
+            // 
+            // txtx
+            // 
+            this.txtx.Location = new System.Drawing.Point(138, 110);
+            this.txtx.Name = "txtx";
+            this.txtx.Size = new System.Drawing.Size(131, 29);
+            this.txtx.TabIndex = 6;
+            this.txtx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtraiz
+            // 
+            this.txtraiz.Location = new System.Drawing.Point(169, 247);
+            this.txtraiz.Name = "txtraiz";
+            this.txtraiz.Size = new System.Drawing.Size(131, 29);
+            this.txtraiz.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(420, 317);
+            this.Controls.Add(this.txtraiz);
+            this.Controls.Add(this.txtx);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncerrar);
+            this.Controls.Add(this.btncalc);
+            this.Controls.Add(this.btnlimpiar);
+            this.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Button btncalc;
+        private System.Windows.Forms.Button btncerrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtx;
+        private System.Windows.Forms.TextBox txtraiz;
+    }
+}
+
---
+++ Raiz Cuadrada/Raiz Cuadrada/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Raiz_Cuadrada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtraiz.Clear();
+            txtx.Clear();
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            int x;
+            double raiz;
+            x = Convert.ToInt32(txtx.Text);
+
+            raiz = Math.Sqrt(x);
+            txtraiz.Text = raiz.ToString();
+        }
+    }
+}
