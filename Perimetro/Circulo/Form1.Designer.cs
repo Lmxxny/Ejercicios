40f7e74bd588e6373aafc7ba544f50d3e4f6951a
Park Jihoon <trabajostodo0@gmail.com>
2024-05-05 08:50:58 -0600

    proyecto 2

---
+++ Perimetro/Circulo/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Circulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtrad = new System.Windows.Forms.TextBox();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.btncalc = new System.Windows.Forms.Button();
+            this.btncerrar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtarea = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtperi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 14.26415F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calcular el Área";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Radio";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtrad
+            // 
+            this.txtrad.Location = new System.Drawing.Point(214, 65);
+            this.txtrad.Name = "txtrad";
+            this.txtrad.Size = new System.Drawing.Size(98, 33);
+            this.txtrad.TabIndex = 2;
+            this.txtrad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnlimpiar.Location = new System.Drawing.Point(74, 126);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(103, 44);
+            this.btnlimpiar.TabIndex = 3;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = false;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // btncalc
+            // 
+            this.btncalc.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncalc.Location = new System.Drawing.Point(183, 126);
+            this.btncalc.Name = "btncalc";
+            this.btncalc.Size = new System.Drawing.Size(103, 44);
+            this.btncalc.TabIndex = 4;
+            this.btncalc.Text = "Calcular";
+            this.btncalc.UseVisualStyleBackColor = false;
+            this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
+            // 
+            // btncerrar
+            // 
+            this.btncerrar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncerrar.Location = new System.Drawing.Point(292, 126);
+            this.btncerrar.Name = "btncerrar";
+            this.btncerrar.Size = new System.Drawing.Size(103, 44);
+            this.btncerrar.TabIndex = 5;
+            this.btncerrar.Text = "Cerrar";
+            this.btncerrar.UseVisualStyleBackColor = false;
+            this.btncerrar.Click += new System.EventHandler(this.btncerrar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(209, 294);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Área";
+            // 
+            // txtarea
+            // 
+            this.txtarea.Location = new System.Drawing.Point(125, 323);
+            this.txtarea.Name = "txtarea";
+            this.txtarea.Size = new System.Drawing.Size(226, 33);
+            this.txtarea.TabIndex = 7;
+            this.txtarea.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(121, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(230, 22);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Entonces el área del círculo...";
+            // 
+            // txtperi
+            // 
+            this.txtperi.Location = new System.Drawing.Point(125, 258);
+            this.txtperi.Name = "txtperi";
+            this.txtperi.Size = new System.Drawing.Size(226, 33);
+            this.txtperi.TabIndex = 9;
+            this.txtperi.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(185, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 26);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Perímetro";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(466, 402);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtperi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtarea);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btncerrar);
+            this.Controls.Add(this.btncalc);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.txtrad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Constantia", 14.26415F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtrad;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Button btncalc;
+        private System.Windows.Forms.Button btncerrar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtarea;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtperi;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Perimetro/Circulo/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Circulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtarea.Clear();
+            txtrad.Clear();
+            txtperi.Clear();
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            int radio;
+            double perimetro;
+            double area;
+            radio = Convert.ToInt32(txtrad.Text);
+
+           perimetro = 2 * Math.PI * radio;
+            area = Math.PI * Math.Pow(radio, 2);
+            txtperi.Text = perimetro.ToString();
+            txtarea.Text = area.ToString(); 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
